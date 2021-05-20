213326081f07b06a9a31ab0e249fbc4c09ece247
Sander ten Kate <sjtenkate@live.nl>
2021-05-20 21:33:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,14 @@ soup = BeautifulSoup(page.content, 'html.parser')
 # get current status string from page-status element
 current_status = soup.find('div', class_='page-status').get_text(strip=True)
 
+# define no reported issue in a variable
 system_has_no_issues = "All Systems Operational"
 
+# set current time
 now = datetime.now()
 
-if current_status in system_has_no_issues:
+# check if current status equals system has no issues & report back
+if current_status == system_has_no_issues:
     print(now, ": SmartRecruiters reports no issues at this time.")
 else:
     print(now, ": ALARM: SmartRecruiters has reported issues! Visit http://status.smartrecruiters.com/ for more information.")
