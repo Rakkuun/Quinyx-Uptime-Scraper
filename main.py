8a603b0fd5a776c294fd4a5af3b4384fc4bf195f
Sander ten Kate <sjtenkate@live.nl>
2021-05-29 19:26:30 +0200

    made mails work

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
+from sendmail import send_status_email
 
 # get HTML from SmartRecruiters status page
 page = requests.get("http://status.smartrecruiters.com/")
@@ -22,5 +23,5 @@ now = datetime.now()
 if current_status == system_has_no_issues:
     print(now, ": SmartRecruiters reports no issues at this time.")
 else:
-    #todo send email via SendGrid if SmartRecruiters has reported issues.
+    send_status_email()
     print(now, ": ALARM: SmartRecruiters has reported issues! Visit http://status.smartrecruiters.com/ for more information.")
--- sendmail.py
+++ sendmail.py
@@ -2,16 +2,18 @@ import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
-message = Mail(
-    from_email='sjtenkate@live.nl',
-    to_emails='sander.tenkate@coolblue.nl',
-    subject='Sending with Twilio SendGrid is Fun',
-    html_content='<strong>and easy to do anywhere, even with Python</strong>')
-try:
-    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-    response = sg.send(message)
-    print(response.status_code)
-    print(response.body)
-    print(response.headers)
-except Exception as e:
-    print(e.body)
+
+def send_status_email():
+    message = Mail(
+        from_email='sjtenkate@live.nl',
+        to_emails='sander.tenkate@coolblue.nl',
+        subject='One or more HR Systems report issues',
+        html_content='<strong>Check if current HR Systems are operational.</strong>')
+    try:
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(e.body)
